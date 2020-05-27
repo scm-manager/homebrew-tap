35da450c0a29fa28721b40be3fb0aca301e44e94
Sebastian Sdorra <s.sdorra@gmail.com>
2020-05-27 06:55:06 +0200

    update scm-manager to version 2.0.0-rc8

--- Formula/scm-server.rb
+++ Formula/scm-server.rb
@@ -1,8 +1,8 @@
 class ScmServer < Formula
   desc "The easiest way to share and manage your Git, Mercurial and Subversion repositories"
   homepage "https://scm-manager.org"
-  url "https://packages.scm-manager.org/repository/public/sonia/scm/scm-server/2.0.0-rc7/scm-server-2.0.0-rc7-app.tar.gz"
-  sha256 "3f4b16b89b131c76a2bba9935490738b7c02c002dc446248cd388f7a8f086c94"
+  url "https://packages.scm-manager.org/repository/public/sonia/scm/scm-server/2.0.0-rc8/scm-server-2.0.0-rc8-app.tar.gz"
+  sha256 "800ed5a282062176c08866cfb58c5c9da37d2781d9041eead3560b4a4268f54a"
 
   depends_on "openjdk@11"
   conflicts_with "scm-manager", :because => "both install the same binaries"
