6058e583a4bdfac0d1633de13af5406a95759e05
CES Marvin <cesmarvin@cloudogu.com>
2025-08-01 10:28:50 +0000

    Update scm-server formula to version 3.9.0

--- Formula/scm-server.rb
+++ Formula/scm-server.rb
@@ -1,8 +1,8 @@
 class ScmServer < Formula
   desc "Share and manage your Git, Mercurial and Subversion repositories"
   homepage "https://scm-manager.org"
-  url "https://packages.scm-manager.org/repository/releases/sonia/scm/packaging/unix/3.8.0/unix-3.8.0.tar.gz"
-  sha256 "35b73129262e925d8aef1a1760dbd7194dccc84aebd87e86cece2d6d88d38d78"
+  url "https://packages.scm-manager.org/repository/releases/sonia/scm/packaging/unix/3.9.0/unix-3.9.0.tar.gz"
+  sha256 "701cc8979232e26170978ec0c90ac31796b68a4393f3d9baa2f27ea2c657342d"
 
   depends_on "openjdk@17"
   conflicts_with "scm-manager", because: "both install the same binaries"
